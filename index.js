b6b427ca5434ee20eaa9363a74966ef267851a7e
henoheTK <takeru.hawk.son@gmail.com>
2020-11-10 17:44:16 +0900

    いろいろと実験とかできるように

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 $('#happy-button').on('click',function(){
-  $('#right-eye').attr('src', './images/happy-right-eye.png');
-  $('#left-eye').attr('src', './images/happy-left-eye.png');
-  $('#mouth-image').attr('src', './images/happy-mouth.png');
+  $('.right-eye').attr('src', './images/happy-right-eye.png');
+  $('.left-eye').attr('src', './images/happy-left-eye.png');
+  $('.mouth-image').attr('src', './images/happy-mouth.png');
 });
 $('#nomal-button').on('click',function(){
-  $('#right-eye').attr('src', './images/nomal-right-eye.png');
-  $('#left-eye').attr('src', './images/nomal-left-eye.png');
-  $('#mouth-image').attr('src', './images/nomal-mouth.png');
+  $('.right-eye').attr('src', './images/nomal-right-eye.png');
+  $('.left-eye').attr('src', './images/nomal-left-eye.png');
+  $('.mouth-image').attr('src', './images/nomal-mouth.png');
 });
 $('#angry-button').on('click',function(){
-  $('#right-eye').attr('src', './images/angry-right-eye.png');
-  $('#left-eye').attr('src', './images/angry-left-eye.png');
-  $('#mouth-image').attr('src', './images/angry-mouth.png');
+  $('.right-eye').attr('src', './images/angry-right-eye.png');
+  $('.left-eye').attr('src', './images/angry-left-eye.png');
+  $('.mouth-image').attr('src', './images/angry-mouth.png');
 });
+$( 'input[name="emotion"]:radio' ).change( function() {
+  var radioval = $(this).val();
+  $('.right-eye').attr('src', './images/'+radioval+'-right-eye.png');
+  $('.left-eye').attr('src', './images/'+radioval+'-left-eye.png');
+  $('.mouth-image').attr('src', './images/'+radioval+'-mouth.png');
+});  
+
 
 $(window).on('load',function(){
-  randomMoveTimer('#face-image'  , 1000);
-  randomMoveTimer('#hair-image'  , 1000);
-  randomMoveTimer('#nose-image'  , 1000);
-  randomMoveTimer('#mouth-image' , 1000);
-  randomMoveTimer('#left-eye'    , 1000);
-  randomMoveTimer('#right-eye'   , 1000);
+  for(let i = 0; i <100; i++){
+    let div=
+    $('<div class="face">'+
+    '<img class="face-image" src="./images/face.png"></img>'+
+      '<img class="hair-image" src="./images/hair.png"></img>'+
+      '<img class="left-eye" src="./images/nomal-left-eye.png"></img>'+
+      '<img class="right-eye" src="./images/nomal-right-eye.png"></img>'+
+      '<img class="mouth-image" src="./images/nomal-mouth.png"></img>'+
+      '<img class="nose-image" src="./images/noma-nose.png"></img>'+
+    '</div>');
+
+    randomMoveTimer(div , 1000);
+
+    $('#faces').append(div);
+  }
 });
 
-function randomMoveTimer(name , time){
-  let moveMax=10;
+function randomMoveTimer(object , time){
+  let moveMax=5;
   setInterval(function(){
-    randomMove(name , moveMax , moveMax);
+    randomMove(object , moveMax , moveMax);
   } , time);
 };
 
-function randomMove(name , topmax , leftmax){
-  $(name).css('top' , getRandomInt( topmax ) + 'px');
-  $(name).css('left' , getRandomInt( leftmax ) + 'px');
+function randomMove(object , topmax , leftmax){
+  object.css('top' , getRandomInt( topmax ) + 'px');
+  object.css('left' , getRandomInt( leftmax ) + 'px');
 }
 
 function getRandomInt(max) {
